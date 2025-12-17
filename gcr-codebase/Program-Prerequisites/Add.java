9564fdf6115f43faa7c3be7b18847d052f52d2a7
Saud Ansari <165356306+Saud0786@users.noreply.github.com>
2025-12-17 11:12:59 +0530

    [Saud]: Added Addition of two number
    Added prompts for user input before reading numbers.

--- gcr-codebase/Program-Prerequisites/Add.java
+++ gcr-codebase/Program-Prerequisites/Add.java
@@ -3,9 +3,12 @@ public class Add
 {
   public static void main(String args[]){
      Scanner sc = new Scanner(System.in);
+     System.out.println("Enter first num: " ); 
      int num1 = sc.nextInt(); 
+     System.out.println("Enter Second num: " ); 
      int num2 = sc.nextInt(); 
      int sum = num1+num2;
      System.out.println("Sum of two num: "+ sum ); 
   }
+
 }
