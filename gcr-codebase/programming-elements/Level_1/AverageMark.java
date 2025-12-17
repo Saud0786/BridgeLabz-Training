ad7bcddab971e198ac7052e15076c7fbde6c4690
Saud0786 <saud31253@gmail.com>
2025-12-17 12:37:26 +0530

    [Saud]: Added Average marks of three subjects

---
+++ gcr-codebase/programming-elements/Level_1/AverageMark.java
@@ -0,0 +1,18 @@
+public class AverageMark{
+ public static void main(String args[]){
+   
+   // Math marks
+    int mathsMark=94;
+   
+   // Physics marks
+    int physicsMark=95;
+    
+   // Chemistry Marks
+    int chemistryMark=96;
+   
+    // Average marks of all subjects
+    int average = (mathsMark + physicsMark + physicsMark)/3;
+    System.out.println("Average Marks of all subjects is: "+ average);  
+   
+ }
+}
