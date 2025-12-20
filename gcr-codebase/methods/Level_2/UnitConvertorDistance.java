06a477c3d9d98a050a33e705254223ba46c1f876
Saud0786 <saud31253@gmail.com>
2025-12-20 19:31:27 +0530

    [Saud]: Added Unit convertor Distance

---
+++ gcr-codebase/methods/Level_2/UnitConvertorDistance.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class UnitConvertorDistance {
+    
+	// Km to Miles
+    public static double convertKmToMiles(double km) {
+        return km * 0.621371;
+    }
+    // Miles to Km 
+    public static double convertMilesToKm(double miles) {
+        return miles * 1.60934;
+    }
+    // Meters to Feet
+    public static double convertMetersToFeet(double meters) {
+        return meters * 3.28084;
+    }
+    // Feet to Meters
+    public static double convertFeetToMeters(double feet) {
+        return feet * 0.3048;
+    }
+	
+}
