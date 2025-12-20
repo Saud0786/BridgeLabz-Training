51f2f20d77c1ee5fa63fc022c06e6c183aa202df
Saud0786 <saud31253@gmail.com>
2025-12-20 19:32:53 +0530

    [Saud]: Added Unit convertor Heights

--- gcr-codebase/methods/Level_2/UnitConvertorYardsToFeet.java
+++ gcr-codebase/methods/Level_2/UnitConvertorYardsToFeet.java
@@ -27,27 +27,5 @@ public class UnitConvertorYardsToFeet {
         return inches * 2.54;
     }
 
-    
-    public static void main(String[] args) {
-        // Create object of Scanner  
-        Scanner sc = new Scanner(System.in);
-
-        // Perform all operation
-        System.out.print("Enter value in Yards: ");
-        double yards = sc.nextDouble();
-        System.out.println("Yards to Feet = " + convertYardsToFeet(yards));
-
-        System.out.print("\nEnter value in Feet: ");
-        double feet = sc.nextDouble();
-        System.out.println("Feet to Yards = " + convertFeetToYards(feet));
-
-        System.out.print("\nEnter value in Meters: ");
-        double meters = sc.nextDouble();
-        System.out.println("Meters to Inches = " + convertMetersToInches(meters));
-
-        System.out.print("\nEnter value in Inches: ");
-        double inches = sc.nextDouble();
-        System.out.println("Inches to Meters = " + convertInchesToMeters(inches));
-        System.out.println("Inches to Centimeters = " + convertInchesToCm(inches));
     }
 }
