b01aefb0dafa4f2fa9bc755c2636cca88bc34cf1
Saud0786 <saud31253@gmail.com>
2025-12-20 19:28:40 +0530

    [Saud]: Added Unit convertor Temperature etc..

---
+++ gcr-codebase/methods/Level_2/UnitConvertorTemperature.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class UnitConvertorTemperature {
+
+    // Method to convert Fahrenheit to Celsius
+    public static double convertFarhenheitToCelsius(double farhenheit) {
+        return (farhenheit - 32) * 5 / 9;
+    }
+
+    // Method to convert Celsius to Fahrenheit
+    public static double convertCelsiusToFarhenheit(double celsius) {
+        return (celsius * 9 / 5) + 32;
+    }
+
+    // Method to convert Pounds to Kilograms
+    public static double convertPoundsToKilograms(double pounds) {
+        return pounds * 0.453592;
+    }
+
+    // Method to convert Kilograms to Pounds
+    public static double convertKilogramsToPounds(double kilograms) {
+        return kilograms * 2.20462;
+    }
+
+    // Method to convert Gallons to Liters
+    public static double convertGallonsToLiters(double gallons) {
+        return gallons * 3.78541;
+    }
+
+    // Method to convert Liters to Gallons
+    public static double convertLitersToGallons(double liters) {
+        return liters * 0.264172;
+    }
+	
+    }
+}
