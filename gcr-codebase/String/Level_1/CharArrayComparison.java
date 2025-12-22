a8866cc437b3a3c502d30110dbbdebf1d7b44a04
Saud0786 <saud31253@gmail.com>
2025-12-22 14:59:07 +0530

    [Saud]: Added Compare char String using two method

---
+++ gcr-codebase/String/Level_1/CharArrayComparison.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+
+public class CharArrayComparison {
+
+    // Method to return characters without using toCharArray()
+    public static char[] getCharacters(String text) {
+        char[] chars = new char[text.length()];
+        for (int i = 0; i < text.length(); i++) {
+            chars[i] = text.charAt(i);
+        }
+        return chars;
+    }
+
+    // Method to compare two character arrays
+    public static boolean compareCharArrays(char[] arr1, char[] arr2) {
+        if (arr1.length != arr2.length) {
+            return false;
+        }
+
+        for (int i = 0; i < arr1.length; i++) {
+            if (arr1[i] != arr2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        // Create object of Scanner
+        Scanner sc = new Scanner(System.in);
+
+        // Taking input
+        System.out.print("Enter the string: ");
+        String text = sc.next();
+
+        // User-defined method
+        char[] userDefinedArray = getCharacters(text);
+
+        // Built-in method
+        char[] builtInArray = text.toCharArray();
+
+        // Display characters from user-defined method
+        System.out.println("\nCharacters using user-defined method:");
+        for (char c : userDefinedArray) {
+            System.out.print(c + " ");
+        }
+
+        // Display characters from built-in method
+        System.out.println("\n\nCharacters using toCharArray():");
+        for (char c : builtInArray) {
+            System.out.print(c + " ");
+        }
+
+        // Compare both arrays
+        boolean result = compareCharArrays(userDefinedArray, builtInArray);
+
+        // Display result
+        System.out.println("\n\nAre both character arrays equal? " + result);
+    }
+}
