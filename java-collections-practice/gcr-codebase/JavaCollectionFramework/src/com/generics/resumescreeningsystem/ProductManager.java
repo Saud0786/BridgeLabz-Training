3d8e41da43af47d7045ea28e239b882923c6bbfd
Saud0786 <saud31253@gmail.com>
2026-01-17 16:50:00 +0530

    [Saud] : added AI-Driven Resume Screening System

---
+++ java-collections-practice/gcr-codebase/JavaCollectionFramework/src/com/generics/resumescreeningsystem/ProductManager.java
@@ -0,0 +1,11 @@
+package com.generics.resumescreeningsystem;
+
+// Product Manager role
+public class ProductManager extends JobRole{
+    public ProductManager(){
+        super("Product Manager");
+    }
+    public void evaluateSkills(){
+        System.out.println("Evaluating Product Strategy, Communication");
+    }
+}
