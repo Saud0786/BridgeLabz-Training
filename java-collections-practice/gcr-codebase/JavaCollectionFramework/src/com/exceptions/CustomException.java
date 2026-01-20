bed1c369fb410b61653637397608ff4e64da20ed
Saud0786 <saud31253@gmail.com>
2026-01-21 00:24:38 +0530

    [Saud] : Added Custom Exception printing

---
+++ java-collections-practice/gcr-codebase/JavaCollectionFramework/src/com/exceptions/CustomException.java
@@ -0,0 +1,35 @@
+package com.exceptions;
+import java.util.*;
+
+// Custom Exception
+class InvalidAgeException extends Exception {
+    InvalidAgeException(String message) {
+        super(message);
+    }
+}
+
+public class CustomException {
+
+    // Method to validate age
+    static void validateAge(int age) throws InvalidAgeException {
+        if (age < 18) {
+            throw new InvalidAgeException("Age must be 18 or above");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            System.out.print("Enter age: ");
+            int age = sc.nextInt();
+
+            validateAge(age); 
+            System.out.println("Access granted!");
+
+        } catch (InvalidAgeException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
