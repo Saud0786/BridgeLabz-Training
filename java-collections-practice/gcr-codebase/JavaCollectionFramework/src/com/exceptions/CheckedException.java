b8d022ea8155ca52c58d5500a0c08207bb05e630
Saud0786 <saud31253@gmail.com>
2026-01-21 00:22:07 +0530

    [Saud] : Added Checked Eception handle

---
+++ java-collections-practice/gcr-codebase/JavaCollectionFramework/src/com/exceptions/CheckedException.java
@@ -0,0 +1,23 @@
+package com.exceptions;
+import java.io.*;
+
+public class CheckedException {
+
+    public static void main(String[] args) {
+
+        try {
+            FileReader file = new FileReader("data.txt");
+            BufferedReader br = new BufferedReader(file);
+
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+
+            br.close();
+
+        } catch (IOException e) {
+            System.out.println("File not found");
+        }
+    }
+}
