7f0a867f10a62c823f6c36d655dabfcff7338f48
Saud0786 <saud31253@gmail.com>
2026-01-21 00:18:39 +0530

    [Saud] : Added Try with resources handling

---
+++ java-collections-practice/gcr-codebase/JavaCollectionFramework/src/com/exceptions/TryWithResources.java
@@ -0,0 +1,16 @@
+package com.exceptions;
+import java.io.*;
+
+public class TryWithResources {
+
+    public static void main(String[] args) {
+
+        try (BufferedReader br = new BufferedReader(new FileReader("info.txt"))) {
+            String line = br.readLine();
+            System.out.println(line);
+
+        } catch (IOException e) {
+            System.out.println("Error reading file");
+        }
+    }
+}
